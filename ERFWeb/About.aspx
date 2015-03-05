a5b695ce7d5137fc3ef19797c981746b31579ff7
YeongYuh <yyxuster@gmail.com>
2015-03-05 21:21:57 +0800

    Revert "remove .net framework 4 applications"
    This reverts commit a65c083637578c67c17f211ab720b17bf95dae59.

---
+++ ERFWeb/About.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="關於我們" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="About.aspx.cs" Inherits="ERFWebApplication.About" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    
+<table cellspacing="0" cellpadding="5" border="0" style="background-color: transparent;">
+<tr>
+  <td style="width:70%; vertical-align:top; font-size: 16px;">
+  　為提高醫療網救護之品質及安全， 本系統考慮系統動態流程的觀念，快速地即時反應網絡內各醫院之病患相互轉院影響之動態病床使用資訊。網絡醫院病床預測資訊己考慮時間的概念，將可提高病床資訊的精準度，使病患到院後確切有病床的機率提高，也更有效率的資源分配規畫。本系統有以下特色：
+<ul style="list-style-type:circle">
+<li>可預測及顯示醫院各類空床數量資訊在未來20, 40, 60分鐘等區間。</li>
+<li>提高資訊的精準度→醫院資訊考慮”時間”的概念，病患到院後確切有空床的機率提高。</li>
+<li>更有效率的資源規畫→若傷病情況超乎該院醫療資源或能力可以處置，則可盡早轉介至其他醫療院所或醫學中心，以節省時間。</li>
+<li>系統動態流程的觀念→快速地反應網絡內各醫院之病患相互轉院影響之空床數量資訊變化。</li>
+</ul>
+
+
+<p style="text-align:left;"> 累計瀏覽人數：<img src="http://counter1.fc2.com/counter_img.php?id=27501949&main=1" alt="odsm logo" /></p>
+
+</td>
+<td style="width:30%; background-color:#fff; text-align:left; vertical-align:bottom; color:#000;" >
+<img src="img/logo2.jpg" width="75%" alt="logo" /><br/>
+最佳決策系統研究團隊
+<ul style="list-style-type:circle">
+<li>東海大學工業工程與經營資訊系 <a href="http://web.thu.edu.tw/sjweng/www/" target="_blank" style="color: #CC0000">翁紹仁</a>老師</li>
+<li>弘光科技大學資訊工程系 <a href="http://csie.hk.edu.tw/info/73" target="_blank" style="color: #CC0000">徐永煜</a>老師</li>
+<li>台中榮民總醫院嘉義分院 <a href="http://www.vhcy.gov.tw/vhcy/index.php?module=CurerInfo&action=user_detail&sn=693" target="_blank" style="color: #CC0000">王立敏</a>副院長（前中區緊急醫療應變中心執行長）</li>
+</ul>
+
+</td>
+
+</tr>
+</table>
+
+</asp:Content>
---
+++ ERFWindowsService/Service1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using InfluxDB;
+using System.Threading;
+using System.Timers;
+using ERFWindowsService.ServiceReference1;
+
+namespace ERFWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private System.Timers.Timer timer1 = null;
+        public Service1()
+        {
+            InitializeComponent();
+            this.AutoLog = false;
+            if(!System.Diagnostics.EventLog.SourceExists("ERFSource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "ERFSource", "ERFLog");
+            }
+            eventLog1.Source = "ERFSource";
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("ERF Start.");
+            RunForcasting();
+            timer1 = new System.Timers.Timer();
+            timer1.Elapsed += new ElapsedEventHandler(timer1_Elapsed);
+            timer1.Interval = 420000;
+            timer1.Start(); 
+        }
+
+        protected override void OnStop()
+        {
+
+            timer1.Stop();
+            timer1 = null;
+            eventLog1.WriteEntry("ERF Stop.");
+        }
+
+        private void timer1_Elapsed(object sender, EventArgs e)
+        {
+            //RunGetERInfo();
+            eventLog1.WriteEntry("Forcast.");
+            while (true)
+            {
+                try
+                {
+                    timer1.Stop();
+                    RunForcasting();
+                    timer1.Start();
+                    break;
+
+                }
+                catch (Exception ex)
+                {
+                    //errortextBox.Text += "/r/n " + DateTime.Now.TimeOfDay + "/r/n" + ex.ToString();
+                    eventLog1.WriteEntry("Exception:" + ex.ToString());
+                    Thread.Sleep(10000);
+                }
+            }
+
+        }
+
+        private void RunForcasting()
+        {
+            //Initial Database
+            List<Forecast_data> list = null;
+            //InfluxDBClient client = new InfluxDBClient("210.240.226.146", 8086, "ERFadmin", "eRfe19", "ERFdbHistory");
+            //eventLog1.WriteEntry("1");//grainflux.cloudapp.net
+            InfluxDBClient clientshow = new InfluxDBClient("59.126.164.5", 8086, "ERFadmin", "eRfe19", "ERFdb");
+            InfluxDBClient client = new InfluxDBClient("59.126.164.5", 8086, "ERFadmin", "eRfe19", "ERFdbHistory");
+            //eventLog1.WriteEntry("2");
+            // Create 
+            var serie = new Serie
+            {
+                ColumnNames = new[] { "time", "flownum","Forecast_Time", "HpF_BED", "HpF_BED_Lower", "HpF_BED_Upper"
+                                                     , "HpF_WARN", "HpF_WARN_Lower", "HpF_WARN_Upper"
+                                                     , "HpF_ICU", "HpF_ICU_Lower", "HpF_ICU_Upper"}
+            };
+            var series = new List<Serie> { serie };
+            var serieshow = new List<Serie>();
+            //eventLog1.WriteEntry("3");
+            ///Initial Web Service
+            ERFServiceClient AS = new ERFServiceClient("BasicHttpBinding_IERFService1");
+            AS.InnerChannel.OperationTimeout = new TimeSpan(0, 7, 0);
+            //eventLog1.WriteEntry("4");
+
+            string FlowNum = Convert.ToInt64(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds).ToString();
+            //eventLog1.WriteEntry("5");
+            //20 miniutes
+            //client = new InfluxDBClient("210.240.226.146", 8086, "ERFadmin", "eRfe19", "ERFdb");
+            string[] results = Simul8Forcasting(AS, 15, 20.0);
+            //eventLog1.WriteEntry("6");
+            if (results != null)
+            {
+                list = (List<Forecast_data>)JsonConvert.DeserializeObject(results[2], typeof(List<Forecast_data>));
+
+                foreach (Forecast_data data in list)
+                {
+                    serie.Name = String.Copy((string)data.Hospital_ID);
+                    serie.Points.Add(new object[] { data.time, FlowNum, data.Forecast_Time, data.HpF_BED[0], data.HpF_BED[1],data.HpF_BED[2]
+                                                                  , data.HpF_WARN[0],data.HpF_WARN[1],data.HpF_WARN[2] 
+                                                                  , data.HpF_ICU[0],data.HpF_ICU[1],data.HpF_ICU[2]});
+                    //client.Insert(series);
+                    Thread.Sleep(200);
+                    serie.Points.Clear();
+
+                    var serieh = new Serie
+                    {
+                        ColumnNames = new[] { "time", "flownum","Forecast_Time", "HpF_BED", "HpF_BED_Lower", "HpF_BED_Upper"
+                                                     , "HpF_WARN", "HpF_WARN_Lower", "HpF_WARN_Upper"
+                                                     , "HpF_ICU", "HpF_ICU_Lower", "HpF_ICU_Upper"},
+                        Name = String.Copy((string)data.Hospital_ID)
+                    };
+                    serieh.Points.Add(new object[] { data.time, FlowNum, data.Forecast_Time, data.HpF_BED[0], data.HpF_BED[1],data.HpF_BED[2]
+                                                                  , data.HpF_WARN[0],data.HpF_WARN[1],data.HpF_WARN[2] 
+                                                                  , data.HpF_ICU[0],data.HpF_ICU[1],data.HpF_ICU[2]});
+                    serieshow.Add(serieh);
+
+                }
+            }
+
+            //40 miniutes
+            //client = new InfluxDBClient("210.240.226.146", 8086, "ERFadmin", "eRfe19", "ERFdb");
+            results = Simul8Forcasting(AS, 15, 40.0);
+            if (results != null)
+            {
+                list = (List<Forecast_data>)JsonConvert.DeserializeObject(results[2], typeof(List<Forecast_data>));
+                foreach (Forecast_data data in list)
+                {
+                    serie.Name = String.Copy((string)data.Hospital_ID);
+                    serie.Points.Add(new object[] { data.time, FlowNum, data.Forecast_Time, data.HpF_BED[0], data.HpF_BED[1],data.HpF_BED[2]
+                                                                  , data.HpF_WARN[0],data.HpF_WARN[1],data.HpF_WARN[2] 
+                                                                  , data.HpF_ICU[0],data.HpF_ICU[1],data.HpF_ICU[2]});
+                    //client.Insert(series);
+                    Thread.Sleep(200);
+                    serie.Points.Clear();
+
+                    var serieh = new Serie
+                    {
+                        ColumnNames = new[] { "time", "flownum","Forecast_Time", "HpF_BED", "HpF_BED_Lower", "HpF_BED_Upper"
+                                                     , "HpF_WARN", "HpF_WARN_Lower", "HpF_WARN_Upper"
+                                                     , "HpF_ICU", "HpF_ICU_Lower", "HpF_ICU_Upper"},
+                        Name = String.Copy((string)data.Hospital_ID)
+                    };
+                    serieh.Points.Add(new object[] { data.time, FlowNum, data.Forecast_Time, data.HpF_BED[0], data.HpF_BED[1],data.HpF_BED[2]
+                                                                  , data.HpF_WARN[0],data.HpF_WARN[1],data.HpF_WARN[2] 
+                                                                  , data.HpF_ICU[0],data.HpF_ICU[1],data.HpF_ICU[2]});
+                    serieshow.Add(serieh);
+                }
+            }
+
+            //Thread.Sleep(1000);
+            //60 miniutes
+            //client = new InfluxDBClient("210.240.226.146", 8086, "ERFadmin", "eRfe19", "ERFdb");
+            results = Simul8Forcasting(AS, 15, 60.0);
+            if (results != null)
+            {
+                list = (List<Forecast_data>)JsonConvert.DeserializeObject(results[2], typeof(List<Forecast_data>));
+                foreach (Forecast_data data in list)
+                {
+                    serie.Name = String.Copy((string)data.Hospital_ID);
+                    serie.Points.Add(new object[] { data.time, FlowNum, data.Forecast_Time, data.HpF_BED[0], data.HpF_BED[1],data.HpF_BED[2]
+                                                                  , data.HpF_WARN[0],data.HpF_WARN[1],data.HpF_WARN[2] 
+                                                                  , data.HpF_ICU[0],data.HpF_ICU[1],data.HpF_ICU[2]});
+                    //client.Insert(series);
+                    Thread.Sleep(200);
+                    serie.Points.Clear();
+
+                    var serieh = new Serie
+                    {
+                        ColumnNames = new[] { "time", "flownum","Forecast_Time", "HpF_BED", "HpF_BED_Lower", "HpF_BED_Upper"
+                                                     , "HpF_WARN", "HpF_WARN_Lower", "HpF_WARN_Upper"
+                                                     , "HpF_ICU", "HpF_ICU_Lower", "HpF_ICU_Upper"},
+                        Name = String.Copy((string)data.Hospital_ID)
+                    };
+                    serieh.Points.Add(new object[] { data.time, FlowNum, data.Forecast_Time, data.HpF_BED[0], data.HpF_BED[1],data.HpF_BED[2]
+                                                                  , data.HpF_WARN[0],data.HpF_WARN[1],data.HpF_WARN[2] 
+                                                                  , data.HpF_ICU[0],data.HpF_ICU[1],data.HpF_ICU[2]});
+                    serieshow.Add(serieh);
+                }
+            }
+
+            for(int i=1;i<=12;i++)
+                clientshow.Query("DROP SERIES H"+i);
+            
+            clientshow.Insert(serieshow);
+            client.Insert(serieshow);
+
+
+            AS.Close();
+
+        }
+
+        private string[] Simul8Forcasting(ERFServiceClient AS, int rep, double Runlength)
+        {
+
+            string tag = "tag=A%2C" + rep.ToString() + "%2C" + Runlength.ToString();
+            string[] results = null;
+
+            //while (true)
+            //{
+
+                results = AS.getvalue(GenerateStreamFromString(tag));
+                if (results[0] == "VALUE")
+                {
+                    //break;
+                }
+                else
+                {
+                    //errortextBox.Text += "/r/n" + DateTime.Now.TimeOfDay + "/r/n" + results[2];
+                    eventLog1.WriteEntry("Error:" + results[2]);
+                    Thread.Sleep(2000);
+                }
+            //}
+
+            return results;
+
+        }
+        private Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+    }
+}
